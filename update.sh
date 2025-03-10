e6d9373ce0646b199884eba694316577bac66b6f
Pulsarorion <grad.phoebe.2r@icloud.com>
2025-03-10 21:38:43 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ sudo -u wireguarduser systemctl stop wg-quick@wg0.service
 # Temporär eingehende und ausgehende Verbindungen erlauben
 sudo ufw default allow incoming
 sudo ufw default allow outgoing
+sudo ufw reload
 
 # System-Update durchführen
 echo "Führe System-Update durch..."
@@ -18,10 +19,12 @@ sudo ufw default deny incoming
 sudo ufw default deny outgoing
 sudo ufw allow out on wg0  # Nur VPN für ausgehenden Verkehr
 sudo ufw allow in on wg0   # VPN für eingehenden Verkehr
+sudo ufw reload
 
 # Docker-Container starten
-echo "Starte alle Docker-Container..."
-sudo -u dockeruser docker start $(sudo -u dockeruser docker ps -q)
+echo "Starte VPN und alle Docker-Container..."
 sudo -u wireguarduser systemctl start wg-quick@wg0.service
+sleep 30
+sudo -u dockeruser docker start $(sudo -u dockeruser docker ps -q)
 
 echo "System und Docker-Container wurden aktualisiert."
