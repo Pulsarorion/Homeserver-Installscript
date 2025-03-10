6ad036f2201aaa015bea4ab559fc84277fac15a9
Pulsarorion <grad.phoebe.2r@icloud.com>
2025-03-10 21:27:33 +0100

    Update server-shutdown.sh

--- server-shutdown.sh
+++ server-shutdown.sh
@@ -4,7 +4,7 @@
 stop_applications() {
     echo "Stoppe alle laufenden Docker-Container..."
     sudo -u dockeruser docker stop $(sudo -u dockeruser docker ps -q)
-    #sudo -u wireguarduser systemctl stop
+    sudo -u wireguarduser systemctl stop wg-quick@wg0.service
 }
 
 # Server herunterfahren
@@ -12,3 +12,7 @@ shutdown_server() {
     echo "Server wird heruntergefahren..."
     sudo shutdown -h now
 }
+
+# Funktionen ausführen
+stop_applications
+shutdown_server
